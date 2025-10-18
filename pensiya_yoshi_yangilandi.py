a9b6f0a33b42f87afba8577ef74bedd3308e1374
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2025-10-18 19:10:52 +0500

    Update and rename pensiya_yoshi_nafaqa.py to pensiya_yoshi_yangilandi.py

---
+++ pensiya_yoshi_yangilandi.py
@@ -0,0 +1,14 @@
+# jins = input("Jins kiriting: ").lower()
+# yosh = int(input("Yosh kiriting: "))
+# if jins == "ayol" and yosh >= 55:
+#     print(f"Buvi.Siz nafaqadasiz\nSiz {yosh - 55} yildan beri nafaqadasiz")
+#     if jins == "ayol" and yosh <= 54 and yosh >= 0 :
+#        print("Mijoz,siz yaqinda nafaqaga chiqasiz")
+# elif jins == "erkak" and yosh >= 60:
+#      print(f"Bobo.Siz nafaqadasiz\nSiz {yosh - 60} yildan beri nafaqadasiz")
+# elif jins == "erkak" and yosh <= 59 and yosh >= 0:
+#     print("Mijoz,siz yaqinda nafaqaga chiqasiz")
+# elif 0 < yosh >= 120:
+#     print("Xatolikga yo'l qo'yildi qaytadan to'g'rilab kirgizing!")
+# else:
+#     print("Xatolik!!!")
